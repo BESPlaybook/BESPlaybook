c29e0131e651d68093fec9a7424bf3f4ce9c98ec
Anne Demey <anne.demey@isobar.com>
2019-03-28 12:52:07 -0600

    Deployed b96900a with MkDocs version: 1.0.4

--- js/theme.js
+++ js/theme.js
@@ -2,6 +2,24 @@
   
   console.log('base:', mkdocs_base_url)
   console.log('current:', mkdocs_page_url)
-  // document.querySelectorAll('bes-header-nav')
+  var navItems = document.querySelectorAll('.bes-playbook-nav li')
+  navItems.forEach(function (el) {
+    var isLocal = window.location.href.indexOf(mkdocs_base_url) === -1;
+    var slug = ''
+    if (isLocal) {
+      slug = window.location.href.split('8000')[1]
+    } else {
+      slug = window.location.href.split(mkdocs_base_url)[0];
+    }
+
+    var section = slug.split('/')[1];
+    var linkSection = el.dataset.url && el.dataset.url.split('/')[1]
+    
+    console.log('>', section, linkSection)
+
+    if (section && linkSection === section) {
+      el.className += ' active'
+    }
+  });
 
 }())
